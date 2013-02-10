a26ebfd92169585db9ad6ec1a6da41ddbe02c7cf
Taylor McCaslin <Taylor4484@gmail.com>
2013-02-10 12:12:05 -0600

    Working Implementation of Sphere Test Case
    Sphere rejected due to time limit exceed

--- RunXML.py
+++ RunXML.py
@@ -13,23 +13,34 @@ def xml_driver (r, w) :
 
     tree = ET.fromstring(tree)
     t_iter = tree.iter()
-
+    
+#creates a list, field, of elements to be searched for
     search = ET.fromstring(search)
     s_iter = search.iter()
-    field = ""
+    field = []
     for child in s_iter:
-        field = field + child.tag
+        field.append(child.tag)
 
-    print (field)
-
-    #print tree
-    ET.dump(tree)
-    
+#creates a list, lst, of memory addresses for the nodes of the tree    
     lst = []
     for child in t_iter:
-        lst.append(child.tag)
-    print(lst)
-    
+        lst.append(child)
+
+#first match
+    match_parent = tree.findall(".//"+field[0])
+
+    found = []
+#    if(match_child != None): print( match_parent)
+    i = 0
+    for j in match_parent:
+        match_child  = match_parent[i].find(".//"+field[1])
+        if(match_child.tag == field[1]): found.append( match_parent[i])
+        i += 1
+    answer = str(len(found)) + "\n"
+    for x in found:
+        answer += str((lst.index(x)+1))+"\n"
+
+    xml_print(w, answer[:-1])
 
 # ------------
 # xml_tokenizer
@@ -76,14 +87,15 @@ def xml_read (r, a) :
 # xml_print
 # -------------
 
-def xml_print (w,) :
+def xml_print (w,s) :
     """
     prints the values of i, j, and v
     w is a writer
 
     """
-    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
-
+    
+    w.write(s)
+#    w.close()
 
 
         
@@ -98,7 +110,8 @@ import xml.etree.ElementTree as ET
 # main
 # ----
 
-file1 = open("RunXML.in", "r")
-file2 = open("RunXML.tmp", "w")
+#file1 = open("RunXML.in", "r")
+#file2 = open("RunXML.tmp", "w")
 
-xml_driver(file1, file2)
+#xml_driver(file1, file2)
+xml_driver(sys.stdin, sys.stdout)
