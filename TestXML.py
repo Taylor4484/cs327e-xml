4b69ff7791153fa72cf888c033784793d1bf85d9
Taylor McCaslin <Taylor4484@gmail.com>
2013-02-13 18:19:04 -0600

    Cleaning up files & Names

--- TestXML.py
+++ TestXML.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 # -------------------------------
-# projects/collatz/TestCollatz.py
+# projects/xml/TestXML.py
 # Copyright (C) 2013
-# Glenn P. Downing
 # -------------------------------
 
 """
@@ -20,21 +19,81 @@ To test the program:
 import StringIO
 import unittest
 
-from RunXML import *
+from XML import xml_tokenizer, xml_driver, xml_match, xml_print
 
 # -----------
 # TestCollatz
 # -----------
 
+class TestXML (unittest.TestCase) :
+    # ----
+    # xml_tokenizer
+    # ----
+
+    def test_tokenizer1 (self) :
+        print("TEST_TOKENIZER #1")
+        s = StringIO.StringIO("<THU><Team></Team><JiaJia><Team></Team></JiaJia></THU><JiaJia></JiaJia>")
+        tree, sep, search = xml_tokenizer(s)
+        self.assert_( tree == "<THU><Team></Team><JiaJia><Team></Team></JiaJia>" ) 
+        self.assert_( sep == "</THU>" )
+        self.assert_( search == "<JiaJia></JiaJia>" )
+
+    def test_tokenizer2 (self) :
+        s = SringIO.StringIO("<Taylor><Holly></Holly></Taylor><Holly></Holly>")
+        tree, sep, search = xml_tokenizer(s)
+        self.assert_( tree == "<Taylor><Holly></Holly>" ) 
+        self.assert_( sep == "</Taylor>" )
+        self.assert_( search == "<Holly></Holly>" )
+
+    def test_tokenizer3 (self) :
+        s = SringIO.StringIO("<THU><Team><thu></thr></Team></THU><JiaJia></JiaJia>")
+        tree, sep, search = xml_tokenizer(s)
+        self.assert_( tree == "<THU><Team><thu></thr></Team>" ) 
+        self.assert_( sep == "</THU>" )
+        self.assert_( search == "<JiaJia></JiaJia>" )
+
+
+    # ----
+    # xml_match
+    # ----
+
+
+    #SETUP INPUT FOR TEST
+    
+    s = StringIO.StringIO("<THU><Team></Team><JiaJia><Team></Team></JiaJia></THU><JiaJia></JiaJia>")
+    tree, sep, search = xml_tokenizer(s)
+    #fix tree
+    tree = ET.fromstring(tree+sep)
+    t_iter = tree.iter()
+    #ET.dump(tree)
+    
+    #creates a list, field, of elements to be searched for
+    s_iter = ET.fromstring(search).iter()
+    field = []
+    for child in s_iter:
+        field.append(child.tag)
+    #print(sep)
+    
+    #creates a list, lst, of memory addresses for the nodes of the tree    
+    lst = []
+    for child in t_iter:
+        lst.append(child)
+    #print(lst)
+
+
+    def test_match1 (self) :
+         test = xml_match(1, match_parent[0], field)
+         assert_(test == True)
+        
 
     # ----
     # xml_driver
     # ----
 
-def test_driver1 (self) :
-    v = xml_driver("<Taylor><Team><ACRush></ACRush><Jelly></Jelly><Cooly></Cooly></Team><JiaJia><Team><Ahyangyi></Ahyangyi><Dragon></Dragon><Cooly><Amber></Amber></Cooly></Team></JiaJia></Taylor><Taylor><Team></Team></Taylor>", w)
-    self.assert_(v == "1\n1")
-"""
+    def test_driver1 (self) :
+        v = xml_driver("<Taylor><Team><ACRush></ACRush><Jelly></Jelly><Cooly></Cooly></Team><JiaJia><Team><Ahyangyi></Ahyangyi><Dragon></Dragon><Cooly><Amber></Amber></Cooly></Team></JiaJia></Taylor><Taylor><Team></Team></Taylor>", w)
+        self.assert_(v == "1\n1")
+
     def test_driver2 (self) :
         v = xml_driver(100, 200)
         self.assert_(v == 125)
@@ -47,15 +106,6 @@ def test_driver1 (self) :
         v = xml_driver(900, 1000)
         self.assert_(v == 174)
 
-    #reversed range
-    def test_driver5 (self) :
-        v = xml_driver(10, 1)
-        self.assert_(v == 20)
-        
-    #zero range
-    def test_driver6 (self) :
-        v = xml_driver(170, 170)
-        self.assert_(v == 11)
 
 
     # -----
@@ -82,57 +132,7 @@ def test_driver1 (self) :
         collatz_print(w, 900, 1000, 174)
         self.assert_(w.getvalue() == "900 1000 174\n")
 
-    def test_driver_1 (self) :
-        
-##Caught 3rd instance
-
-<THU>
-	<Team>
-		<ACRush></ACRush>
-		<Jelly></Jelly>
-		<Cooly></Cooly>
-	</Team>
-	<JiaJia>
-		<Team>
-			<Ahyangyi></Ahyangyi>
-			<Dragon></Dragon>
-			<Cooly><Amber></Amber></Cooly>
-		</Team>
-		<Taylor>
-			<Team>
-				<Cooly></Cooly>
-			</Team>
-		</Taylor>
-	</JiaJia>
-</THU>
-<Team><Cooly></Cooly></Team>
-
-### ignored 3rd instance
-
-<THU>
-	<Team>
-		<ACRush></ACRush>
-		<Jelly></Jelly>
-		<Cooly></Cooly>
-	</Team>
-	<JiaJia>
-		<Team>
-			<Ahyangyi></Ahyangyi>
-			<Dragon></Dragon>
-			<Cooly><Amber></Amber></Cooly>
-		</Team>
-		<Taylor>
-			<Team>
-				<Amber><Cooly></Cooly></Amber>
-			</Team>
-		</Taylor>
-	</JiaJia>
-</THU>
-<Team><Cooly></Cooly></Team>
-
-#tests root case
-<Taylor><Team><ACRush></ACRush><Jelly></Jelly><Cooly></Cooly></Team><JiaJia><Team><Ahyangyi></Ahyangyi><Dragon></Dragon><Cooly><Amber></Amber></Cooly></Team></JiaJia></Taylor><Taylor><Team></Team></Taylor>
-        
+               
     # -----
     # solve
     # -----
@@ -160,12 +160,12 @@ def test_driver1 (self) :
         w = StringIO.StringIO()
         collatz_solve(r, w)
         self.assert_(w.getvalue() == "5881 2389 238\n9169 9347 260\n193 2702 209\n8515 190 262\n")
-        
-"""
+       
+
 # ----
 # main
 # ----
 
 print ("TestXML.py")
 unittest.main()
-print "Done.")
+print ("Done.")
